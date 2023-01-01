9ed2ce6da7beb0fc490201ddf8cc17bfe8432254
BongJaeChoi__ <jjgod0124@gmail.com>
2023-01-01 21:56:24 +0900

    도메인 모듈 초기화 커밋

---
+++ core/domain/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+}
+
+android {
+    namespace = "jay.bongjae.fakeshop.core.domain"
+    compileSdk = 33
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.3.2"
+    }
+}
+
+dependencies {
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.4")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
+}
