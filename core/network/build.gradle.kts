00355c8cc89083a4c1191b65146b55a19a9edb89
BongJaeChoi__ <jjgod0124@gmail.com>
2023-01-01 21:55:35 +0900

    네트워크 모듈 초기화 커밋

---
+++ core/network/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    kotlin("plugin.serialization") version "1.7.20"
+}
+
+android {
+    namespace = "jay.bongjae.fakeshop.core.network"
+    buildFeatures {
+        buildConfig = true
+    }
+    compileSdk = 33
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.3.2"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("com.google.android.material:material:1.7.0")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.4")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
+}
---
+++ core/network/src/main/java/jay/bongjae/network/JayStoreService.kt
@@ -0,0 +1,27 @@
+package jay.bongjae.network
+
+//https://fakestoreapi.com/
+interface JayStoreService {
+    /**
+     * GET
+    /products
+    GET
+    /products/1
+    GET
+    /products/categories
+    GET
+    /products/category/jewelery
+    GET
+    /cart?userId=1
+    GET
+    /products?limit=5
+    POST
+    /products
+    PUT
+    /products/1
+    PATCH
+    /products/1
+    DELETE
+    /products/1
+     */
+}
