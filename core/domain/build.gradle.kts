bf8ddd953aa65fe45fc4cfff4bc34f4175895294
BongJaeChoi <jjgod0124@gmail.com>
2023-01-03 21:51:00 +0900

    디펜던시 쉽게 추가 관리가 가능한 .tomi 파일 추가 ( gradle-wrapper 7.6 버전업)

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     kotlin("kapt")
+    id("com.google.dagger.hilt.android") version "2.44" apply false
 }
 
 android {
