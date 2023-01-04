879fe6bb70d84b96b922d25b010683dd4d60e7f0
BongJaeChoi <jjgod0124@gmail.com>
2023-01-04 23:34:54 +0900

    의존성 주입용 Dispatcher 생성

---
+++ core/common/src/main/java/jay/bongjae/common/JayFakeShopDispatchers.kt
@@ -0,0 +1,12 @@
+package jay.bongjae.common
+
+import javax.inject.Qualifier
+import kotlin.annotation.AnnotationRetention.RUNTIME
+
+@Qualifier
+@Retention(RUNTIME)
+annotation class Dispatcher(val dispatcher: JayFakeShopDispatchers)
+
+enum class JayFakeShopDispatchers {
+    IO
+}
