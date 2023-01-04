56148e379f978119b98e0aafe4c8ce149deaf1c5
BongJaeChoi <jjgod0124@gmail.com>
2023-01-04 23:38:00 +0900

    네트워크 모듈 모델 구현

---
+++ core/network/src/main/java/jay/bongjae/network/model/Rating.kt
@@ -0,0 +1,13 @@
+package jay.bongjae.network.model
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Rating(
+    @SerialName("count")
+    val count: Int,
+    @SerialName("rate")
+    val rate: Double
+)
